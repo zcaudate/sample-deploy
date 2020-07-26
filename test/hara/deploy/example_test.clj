4e552d8099853e3cc4fbbcedde6ffe5129b1fa1b
Chris Zheng <z@caudate.me>
2020-07-26 20:07:17 +0800

    cleanup

--- test/hara/deploy/example_test.clj
+++
@@ -1,7 +0,0 @@
-(ns hara.deploy.example-test
-  (:require [clojure.test :refer :all]
-            [hara.deploy.example :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
